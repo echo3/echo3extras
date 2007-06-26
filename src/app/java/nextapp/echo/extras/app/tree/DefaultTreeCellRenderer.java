0f6d6c0b29eb4224f733849dbca459ca8291f304
d.de.wit <d.de.wit@06d4db22-c32e-0410-96b4-888c99e491d5>
2007-06-26 15:42:36 +0000

    provide a real implementation of DefaultTreeCellRenderer instead of the current debug stuff

--- src/app/java/nextapp/echo/extras/app/tree/DefaultTreeCellRenderer.java
+++ src/app/java/nextapp/echo/extras/app/tree/DefaultTreeCellRenderer.java
@@ -4,12 +4,18 @@ import nextapp.echo.app.Component;
 import nextapp.echo.app.Label;
 import nextapp.echo.extras.app.Tree;
 
+/**
+ * Default <code>TreeCellRenderer</code> implementation.
+ */
 public class DefaultTreeCellRenderer 
 implements TreeCellRenderer {
 
+    /**
+     * @see nextapp.echo.extras.app.tree.TreeCellRenderer#getTreeCellRendererComponent(
+     *      nextapp.echo.extras.app.Tree, java.lang.Object, int, int)
+     */
     public Component getTreeCellRendererComponent(Tree tree, Object value, int column, int row) {
-        //FIXME debug code
-        return new Label(value + ":" + column + "," + row);
+        return value == null ? new Label() : new Label(value.toString());
     }
 
 }
