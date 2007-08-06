9e95117b07f18fbdf321d1d565b1ead657fa5c21
n.beekman <n.beekman@06d4db22-c32e-0410-96b4-888c99e491d5>
2007-08-06 11:38:30 +0000

    Fixed NPE.

--- src/webcontainer/java/nextapp/echo/extras/webcontainer/sync/component/TabPanePeer.java
+++ src/webcontainer/java/nextapp/echo/extras/webcontainer/sync/component/TabPanePeer.java
@@ -167,7 +167,7 @@ implements LazyRenderContainer {
     public void storeInputProperty(Context context, Component component, String propertyName, int index, Object newValue) {
         if (PROPERTY_ACTIVE_TAB.equals(propertyName)) {
             ClientUpdateManager clientUpdateManager = (ClientUpdateManager) context.get(ClientUpdateManager.class);
-            clientUpdateManager.setComponentProperty(component, TabPane.ACTIVE_TAB_INDEX_CHANGED_PROPERTY, getTabIndex(null, (TabPane)component, (String)newValue));
+            clientUpdateManager.setComponentProperty(component, TabPane.ACTIVE_TAB_INDEX_CHANGED_PROPERTY, getTabIndex(context, (TabPane)component, (String)newValue));
         }
     }
     
