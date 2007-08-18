226ca1b716eb29de23c75b772836b8f96a916c13
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2007-08-18 03:25:24 +0000

    RichTextArea rendering the fundamentals.

--- src/webcontainer/java/nextapp/echo/extras/webcontainer/resource/js/Render.RichTextArea.js
+++ src/webcontainer/java/nextapp/echo/extras/webcontainer/resource/js/Render.RichTextArea.js
@@ -8,11 +8,55 @@ ExtrasRender.ComponentSync.RichTextArea.prototype = EchoCore.derive(EchoRender.C
 
 ExtrasRender.ComponentSync.RichTextArea.prototype.renderAdd = function(update, parentElement) {
     this._mainElement = document.createElement("div");
-    this._mainElement.appendChild(document.createTextNode("I'm a rich text area, really!"));
+    
+    // Create tool bar.
+    this._toolbarContainerDivElement = document.createElement("div");
+    this._mainElement.appendChild(this._toolbarContainerDivElement);
+
+    // Create tool bar buttons.
+    this._createToolbarTextButton("bold");
+
+    // Create IFRAME container DIV element.
+    var iframeContainerElement = document.createElement("div");
+    iframeContainerElement.style.border = "1px inset";
+    this._mainElement.appendChild(iframeContainerElement);
+    
+    // Create IFRAME element.
+    this._iframeElement = document.createElement("iframe");
+    this._iframeElement.style.width = this.width ? this.width : "100%";
+    this._iframeElement.style.height = this.height ? this.height: "200px";
+    this._iframeElement.style.border = "0px none";
+
+    iframeContainerElement.appendChild(this._iframeElement);
+
     parentElement.appendChild(this._mainElement);
 };
 
+ExtrasRender.ComponentSync.RichTextArea.prototype._renderPostAdd = function() {
+    var contentDocument = this._iframeElement.contentWindow.document;
+    contentDocument.open();
+    contentDocument.write("<html><body></body></html>");
+    contentDocument.close();
 
+    contentDocument.designMode = "on";
+
+    contentDocument.execCommand("undo", false, null);
+
+    this._renderPostAddComplete = true;
+};
+
+ExtrasRender.ComponentSync.RichTextArea.prototype.renderSizeUpdate = function() {
+    if (!this._renderPostAddComplete) {
+        this._renderPostAdd();
+    }
+};
+
+ExtrasRender.ComponentSync.RichTextArea.prototype._createToolbarTextButton = function(label) {
+    var spanElement = document.createElement("span");
+    spanElement.appendChild(document.createTextNode(label));
+    this._toolbarContainerDivElement.appendChild(spanElement);
+};
+    
 ExtrasRender.ComponentSync.RichTextArea.prototype.renderDispose = function(update) {
     this._mainElement = null;
 };
