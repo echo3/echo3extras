ea3c291a731246fd166627a7ffe73d273407d3e7
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2007-08-30 09:25:41 +0000

    ColorSelect progress (lines/arrows).

--- src/webcontainer/java/nextapp/echo/extras/webcontainer/resource/js/Render.ColorSelect.js
+++ src/webcontainer/java/nextapp/echo/extras/webcontainer/resource/js/Render.ColorSelect.js
@@ -9,6 +9,11 @@ ExtrasRender.ComponentSync.ColorSelect.prototype.renderAdd = function(update, pa
             this.component.getRenderProperty("saturationHeight", ExtrasApp.ColorSelect.DEFAULT_SATURATION_HEIGHT), false);
     var hueWidth = EchoRender.Property.Extent.toPixels(
             this.component.getRenderProperty("hueWidth", ExtrasApp.ColorSelect.DEFAULT_HUE_WIDTH), true);
+
+    var arrowDownImageSrc = this.client.getServiceUrl("EchoExtras.ColorSelect.ArrowDown");
+    var arrowUpImageSrc = this.client.getServiceUrl("EchoExtras.ColorSelect.ArrowUp");
+    var arrowRightImageSrc = this.client.getServiceUrl("EchoExtras.ColorSelect.ArrowRight");
+    var arrowLeftImageSrc = this.client.getServiceUrl("EchoExtras.ColorSelect.ArrowLeft");
     
     // Create container div element, relatively positioned.
     this._containerDivElement = document.createElement("div");
@@ -16,7 +21,7 @@ ExtrasRender.ComponentSync.ColorSelect.prototype.renderAdd = function(update, pa
     this._containerDivElement.style.left = "0px";
     this._containerDivElement.style.top = "0px";
     this._containerDivElement.style.width = (valueWidth + hueWidth + 29) + "px";
-    this._containerDivElement.style.height = (saturationHeight + 36) +"px";
+    this._containerDivElement.style.hxeight = (saturationHeight + 36) +"px";
     this._containerDivElement.style.overflow = "hidden";
     
     // Create saturation / value selector.
@@ -41,6 +46,89 @@ ExtrasRender.ComponentSync.ColorSelect.prototype.renderAdd = function(update, pa
             svGradientImgElement.style.height = saturationHeight + "px";
             this._svDivElement.appendChild(svGradientImgElement);
         }
+    }
+    
+    // Create container for value selecion bar.
+    this._vLineDivElement = document.createElement("div");
+    this._vLineDivElement.style.position = "absolute";
+    this._vLineDivElement.style.left = "2px";
+    this._vLineDivElement.style.top = "0px";
+    this._vLineDivElement.style.width = "11px";
+    this._vLineDivElement.style.height = (saturationHeight + 14) + "px";
+    this._vLineDivElement.style.overflow = "hidden";
+    this._containerDivElement.appendChild(this._vLineDivElement);
+
+    // Create value selection bar top arrow.
+    if (arrowDownImageSrc) {
+        var vLineTopImgElement = document.createElement("img");
+        vLineTopImgElement.src = arrowDownImageSrc;
+        vLineTopImgElement.style.position = "absolute";
+        vLineTopImgElement.style.left = "0px";
+        vLineTopImgElement.style.top = "0px";
+        this._vLineDivElement.appendChild(vLineTopImgElement);
+    }
+    
+    // Create value selection bar line.
+    var vLineBarDivElement = document.createElement("div");
+    vLineBarDivElement.style.position = "absolute";
+    vLineBarDivElement.style.top = "7px";
+    vLineBarDivElement.style.left = "5px";
+    vLineBarDivElement.style.height = saturationHeight + "px";
+    vLineBarDivElement.style.width = "1px";
+    vLineBarDivElement.style.backgroundColor = "#000000";
+    this._vLineDivElement.appendChild(vLineBarDivElement);
+
+    // Create value selection bar bottom arrow.
+    if (arrowUpImageSrc) {
+        var vLineBottomImgElement = document.createElement("img");
+        vLineBottomImgElement.src = arrowUpImageSrc;
+        vLineBottomImgElement.style.position = "absolute";
+        vLineBottomImgElement.style.left = "0px";
+        vLineBottomImgElement.style.top = (saturationHeight + 7) + "px";
+        this._vLineDivElement.appendChild(vLineBottomImgElement);
+    }
+    
+    // Create saturation selection bar container.
+    this._sLineDivElement = document.createElement("div");
+    this._sLineDivElement.style.position = "absolute";
+    this._sLineDivElement.style.left = "0px";
+    this._sLineDivElement.style.top = (saturationHeight + 2) + "px";
+    this._sLineDivElement.style.height = "11px";
+    this._sLineDivElement.style.width = (valueWidth + 14) + "px";
+    this._sLineDivElement.style.overflow = "hidden";
+    this._containerDivElement.appendChild(this._sLineDivElement);
+    
+    // Create saturation selection bar left arrow.
+    if (arrowRightImageSrc) {
+        var sLineLeftImgElement = document.createElement("img");
+        sLineLeftImgElement.src = arrowRightImageSrc;
+        sLineLeftImgElement.style.position = "absolute";
+        sLineLeftImgElement.style.left = "0px";
+        sLineLeftImgElement.style.top = "0px";
+        this._sLineDivElement.appendChild(sLineLeftImgElement);
+    }
+    
+    // Create saturation selection bar line.
+    var sLineBarDivElement = document.createElement("div");
+    sLineBarDivElement.style.position = "absolute";
+    sLineBarDivElement.style.left = "0px";
+    sLineBarDivElement.style.left = "7px";
+    sLineBarDivElement.style.top = "5px";
+    sLineBarDivElement.style.width = valueWidth + "px";
+    sLineBarDivElement.style.height = "1px";
+    sLineBarDivElement.style.fontSize = "1px";
+    sLineBarDivElement.style.borderTop = "1px #000000 solid";
+    sLineBarDivElement.style.lineHeight = "0";
+    this._sLineDivElement.appendChild(sLineBarDivElement);
+
+    // Create saturation selection bar right arrow.
+    if (arrowLeftImageSrc) {
+        var sLineRightImgElement = document.createElement("img");
+        sLineRightImgElement.src = arrowLeftImageSrc;
+        sLineRightImgElement.style.position = "absolute";
+        sLineRightImgElement.style.left = valueWidth + 7 + "px";
+        sLineRightImgElement.style.top = "0px";
+        this._sLineDivElement.appendChild(sLineRightImgElement);
     }
     
     parentElement.appendChild(this._containerDivElement);
