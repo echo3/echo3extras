4237a687a3ecf467ab4249f6ad5d811a6db3e82b
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2007-10-24 09:22:43 +0000

    EchoCore.extend() conversion.

--- src/webcontainer/java/nextapp/echo/extras/webcontainer/resource/js/Serial.Menu.js
+++ src/webcontainer/java/nextapp/echo/extras/webcontainer/resource/js/Serial.Menu.js
@@ -1,74 +1,100 @@
-ExtrasSerial.PropertyTranslator.ItemModel = function() { };
+ExtrasSerial.PropertyTranslator.ItemModel = {
 
-ExtrasSerial.PropertyTranslator.ItemModel._parseIcon = function(client, propertyElement) {
-	var icon = EchoWebCore.DOM.getChildElementByTagName(propertyElement, "icon");
-	if (icon) {
-		return EchoSerial.PropertyTranslator.ImageReference.toProperty(client, icon);
-	}
-	return null;
-};
-
-ExtrasSerial.PropertyTranslator.ItemModel.toProperty = function(client, propertyElement) {
-	var type = propertyElement.getAttribute("t");
-	if (type.indexOf(ExtrasSerial.PROPERTY_TYPE_PREFIX) == 0) {
-		type = type.substring(ExtrasSerial.PROPERTY_TYPE_PREFIX.length);
-	}
-	var translator = ExtrasSerial.PropertyTranslator[type];
-  	if (translator) {
-  		return translator.toProperty(client, propertyElement);
-	} else {
-		throw new Error("Unsupported model type: " + type);
-	}
-};
-
-ExtrasSerial.PropertyTranslator.MenuModel = function() { };
-
-ExtrasSerial.PropertyTranslator.MenuModel.toProperty = function(client, propertyElement) {
-    var id = propertyElement.getAttribute("id");
-    var text = propertyElement.getAttribute("text");
-    var icon = ExtrasSerial.PropertyTranslator.ItemModel._parseIcon(client, propertyElement);
-    var model = new ExtrasApp.MenuModel(id, text, icon);
+    _parseIcon: function(client, propertyElement) {
+    	var icon = EchoWebCore.DOM.getChildElementByTagName(propertyElement, "icon");
+    	if (icon) {
+    		return EchoSerial.PropertyTranslator.ImageReference.toProperty(client, icon);
+    	}
+    	return null;
+    },
     
-    var children = EchoWebCore.DOM.getChildElementsByTagName(propertyElement, "item");
-    for (var i = 0; i < children.length; i++) {
-    	var childElement = children[i];
-    	var subModel = ExtrasSerial.PropertyTranslator.ItemModel.toProperty(client, childElement);
-    	model.addItem(subModel);
-   }
-   return model;
+    toProperty: function(client, propertyElement) {
+    	var type = propertyElement.getAttribute("t");
+    	if (type.indexOf(ExtrasSerial.PROPERTY_TYPE_PREFIX) == 0) {
+    		type = type.substring(ExtrasSerial.PROPERTY_TYPE_PREFIX.length);
+    	}
+    	var translator = ExtrasSerial.PropertyTranslator[type];
+      	if (translator) {
+      		return translator.toProperty(client, propertyElement);
+    	} else {
+    		throw new Error("Unsupported model type: " + type);
+    	}
+    }
 };
 
-ExtrasSerial.PropertyTranslator.OptionModel = function() { };
+ExtrasSerial.PropertyTranslator.MenuModel = {
 
-ExtrasSerial.PropertyTranslator.OptionModel.toProperty = function(client, propertyElement) {
-    var id = propertyElement.getAttribute("id");
-    var text = propertyElement.getAttribute("text");
-    var icon = ExtrasSerial.PropertyTranslator.ItemModel._parseIcon(client, propertyElement);
-    return new ExtrasApp.OptionModel(id, text, icon);
+    toProperty: function(client, propertyElement) {
+        var id = propertyElement.getAttribute("id");
+        var text = propertyElement.getAttribute("text");
+        var icon = ExtrasSerial.PropertyTranslator.ItemModel._parseIcon(client, propertyElement);
+        var model = new ExtrasApp.MenuModel(id, text, icon);
+        
+        var children = EchoWebCore.DOM.getChildElementsByTagName(propertyElement, "item");
+        for (var i = 0; i < children.length; i++) {
+        	var childElement = children[i];
+        	var subModel = ExtrasSerial.PropertyTranslator.ItemModel.toProperty(client, childElement);
+        	model.addItem(subModel);
+       }
+       return model;
+    }
 };
 
-ExtrasSerial.PropertyTranslator.RadioOptionModel = function() { };
+ExtrasSerial.PropertyTranslator.OptionModel = {
 
-ExtrasSerial.PropertyTranslator.RadioOptionModel.toProperty = function(client, propertyElement) {
-    var id = propertyElement.getAttribute("id");
-    var text = propertyElement.getAttribute("text");
-    var icon = ExtrasSerial.PropertyTranslator.ItemModel._parseIcon(client, propertyElement);
-    return new ExtrasApp.RadioOptionModel(id, text, icon);
+    toProperty: function(client, propertyElement) {
+        var id = propertyElement.getAttribute("id");
+        var text = propertyElement.getAttribute("text");
+        var icon = ExtrasSerial.PropertyTranslator.ItemModel._parseIcon(client, propertyElement);
+        return new ExtrasApp.OptionModel(id, text, icon);
+    }
 };
 
-ExtrasSerial.PropertyTranslator.ToggleOptionModel = function() { };
+ExtrasSerial.PropertyTranslator.RadioOptionModel = {
 
-ExtrasSerial.PropertyTranslator.ToggleOptionModel.toProperty = function(client, propertyElement) {
-    var id = propertyElement.getAttribute("id");
-    var text = propertyElement.getAttribute("text");
-    var icon = ExtrasSerial.PropertyTranslator.ItemModel._parseIcon(client, propertyElement);
-    return new ExtrasApp.ToggleOptionModel(id, text, icon);
+    toProperty: function(client, propertyElement) {
+        var id = propertyElement.getAttribute("id");
+        var text = propertyElement.getAttribute("text");
+        var icon = ExtrasSerial.PropertyTranslator.ItemModel._parseIcon(client, propertyElement);
+        return new ExtrasApp.RadioOptionModel(id, text, icon);
+    }
 };
 
-ExtrasSerial.PropertyTranslator.SeparatorModel = function() { };
+ExtrasSerial.PropertyTranslator.ToggleOptionModel = {
 
-ExtrasSerial.PropertyTranslator.SeparatorModel.toProperty = function(client, propertyElement) {
-    return new ExtrasApp.SeparatorModel();
+    toProperty: function(client, propertyElement) {
+        var id = propertyElement.getAttribute("id");
+        var text = propertyElement.getAttribute("text");
+        var icon = ExtrasSerial.PropertyTranslator.ItemModel._parseIcon(client, propertyElement);
+        return new ExtrasApp.ToggleOptionModel(id, text, icon);
+    }
+};
+
+ExtrasSerial.PropertyTranslator.SeparatorModel = {
+
+    toProperty: function(client, propertyElement) {
+        return new ExtrasApp.SeparatorModel();
+    }
+};
+
+ExtrasSerial.PropertyTranslator.MenuStateModel = {
+
+    toProperty: function(client, propertyElement) {
+        var stateModel = new ExtrasApp.MenuStateModel();
+        var children = EchoWebCore.DOM.getChildElementsByTagName(propertyElement, "i");
+        for (var i = 0; i < children.length; i++) {
+            var childElement = children[i];
+            var enabledValue = childElement.getAttribute("enabled");
+            if (enabledValue != null) {
+                stateModel.setEnabled(childElement.getAttribute("id"), enabledValue == "true");
+            }
+            var selectedValue = childElement.getAttribute("selected");
+            if (selectedValue != null) {
+                stateModel.setSelected(childElement.getAttribute("id"), selectedValue == "true");
+            }
+        }
+        return stateModel;
+    }
 };
 
 EchoSerial.addPropertyTranslator("ExtrasSerial.MenuModel", ExtrasSerial.PropertyTranslator.MenuModel);
@@ -76,24 +102,4 @@ EchoSerial.addPropertyTranslator("ExtrasSerial.OptionModel", ExtrasSerial.Proper
 EchoSerial.addPropertyTranslator("ExtrasSerial.RadioOptionModel", ExtrasSerial.PropertyTranslator.RadioOptionModel);
 EchoSerial.addPropertyTranslator("ExtrasSerial.ToggleOptionModel", ExtrasSerial.PropertyTranslator.ToggleOptionModel);
 EchoSerial.addPropertyTranslator("ExtrasSerial.SeparatorModel", ExtrasSerial.PropertyTranslator.SeparatorModel);
-
-ExtrasSerial.PropertyTranslator.MenuStateModel = function() { };
-
-ExtrasSerial.PropertyTranslator.MenuStateModel.toProperty = function(client, propertyElement) {
-	var stateModel = new ExtrasApp.MenuStateModel();
-    var children = EchoWebCore.DOM.getChildElementsByTagName(propertyElement, "i");
-    for (var i = 0; i < children.length; i++) {
-    	var childElement = children[i];
-	    var enabledValue = childElement.getAttribute("enabled");
-	    if (enabledValue != null) {
-			stateModel.setEnabled(childElement.getAttribute("id"), enabledValue == "true");
-	    }
-    	var selectedValue = childElement.getAttribute("selected");
-    	if (selectedValue != null) {
-	   		stateModel.setSelected(childElement.getAttribute("id"), selectedValue == "true");
-    	}
-	}
-    return stateModel;
-};
-
 EchoSerial.addPropertyTranslator("ExtrasSerial.MenuStateModel", ExtrasSerial.PropertyTranslator.MenuStateModel);
