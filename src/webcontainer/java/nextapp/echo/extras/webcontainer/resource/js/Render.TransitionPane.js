ccb823bfe6ffd4bb8ecf57bceda063a3f3f0c02f
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2007-11-22 03:59:37 +0000

    More progress on TransitionPane...still totally broken.

--- src/webcontainer/java/nextapp/echo/extras/webcontainer/resource/js/Render.TransitionPane.js
+++ src/webcontainer/java/nextapp/echo/extras/webcontainer/resource/js/Render.TransitionPane.js
@@ -5,8 +5,9 @@ ExtrasRender.ComponentSync.TransitionPane = Core.extend(EchoRender.ComponentSync
     },
 
     _element: null,
-    _transitionDuration: -1,
+    _transitionDuration: null,
     _transition: null,
+    _runnable: null,
     
     /**
      * The element containing the old child element, which is being transitioned FROM.
@@ -14,9 +15,9 @@ ExtrasRender.ComponentSync.TransitionPane = Core.extend(EchoRender.ComponentSync
     oldChildDivElement: null,
     
     /**
-     * The element containing the new child element, which is being transitioned TO.
+     * The element containing the current/new child element, which is being transitioned TO.
      */
-    newChildDivElement: null,
+    childDivElement: null,
     
     /**
      * Flag indicating whether initial content has been loaded (no transition effect is ued on the first load).
@@ -32,27 +33,53 @@ ExtrasRender.ComponentSync.TransitionPane = Core.extend(EchoRender.ComponentSync
             this.oldChildDivElement = null;
         }
         
-        if (this.newChildDivElement) {
-            this.newChildDivElement.style.display = "block";
-            this.newChildDivElement = null;
+        if (this.childDivElement) {
+            this.childDivElement.style.display = "block";
+        }
+    },
+
+    _loadTransition: function() {
+        switch (this.component.getRenderProperty("transition")) {
+        case ExtrasApp.TransitionPane.TYPE_FADE:
+            this._transition = new ExtrasRender.ComponentSync.TransitionPane.FadeOpacityTransition();
+            break;
+        default:
+            this._transition = null;
+            this._transitionDuration = null;
+        }
+        
+        if (this._transition) {
+            this._transitionDuration = this.component.getRenderProperty("duartion", 
+                    this._transition.constructor.DEFAULT_TRANSITION_DURATION);
         }
     },
 
     renderAdd: function(update, parentElement) {
+        this._loadTransition();
         this._element = document.createElement("div");
         this._element.style.cssText = "position:absolute;overflow:hidden;top:0;left:0;width:100%;height:100%;";
         parentElement.appendChild(this._element);
+        if (this.component.children.length > 0) {
+            this._renderAddChild(update);
+        }
     },
     
-    _renderAddChild: function() {
-        this.newChildDivElement = document.createElement("div");
-        this.newChildDivElement.style.cssText = "position:absolute;top:0;left:0;width:100%;height:100%;";
+    _renderAddChild: function(update) {
+        this.childDivElement = document.createElement("div");
+        this.childDivElement.style.cssText = "position:absolute;top:0;left:0;width:100%;height:100%;";
         if (this._initialContentLoaded) {
-            this.newChildDivElement.style.display = "none";
+            this.childDivElement.style.display = "none";
         } else {
             this._initialContentLoaded = true;
         }
-        this._element.appendChild(this.newChildDivElement);
+        EchoRender.renderComponentAdd(update, this.component.children[0], this.childDivElement);
+        this._element.appendChild(this.childDivElement);
+        
+        if (this._transition) {
+            this._startTransition();
+        } else {
+            this.doImmediateTransition();
+        }
     },
     
     renderDisplay: function() {
@@ -64,9 +91,85 @@ ExtrasRender.ComponentSync.TransitionPane = Core.extend(EchoRender.ComponentSync
     },
 
     renderUpdate: function(update) {
-        return true;
+        var fullRender = false;
+        if (update.hasUpdatedProperties() || update.hasUpdatedLayoutDataChildren()) {
+            // Full render
+            fullRender = true;
+        } else {
+            var removedChildren = update.getRemovedChildren();
+            if (removedChildren) {
+                // Remove children.
+                this.oldChildDivElement = this.childDivElement;
+                this.childDivElement = null;
+            }
+            var addedChildren = update.getAddedChildren();
+            if (update.parent.children > 1) {
+                throw new Error("Cannot render more than one child in a TransitionPane.");
+            }
+            
+            if (addedChildren) {
+                // Add children.
+                this._renderAddChild(update); 
+            }
+        }
+        if (fullRender) {
+            var element = this._element;
+            var containerElement = element.parentNode;
+            EchoRender.renderComponentDispose(update, update.parent);
+            containerElement.removeChild(element);
+            this.renderAdd(update, containerElement);
+        }
+        
+        return fullRender;
+    },
+    
+    _startTransition: function() {
+        this._runnable = new ExtrasRender.ComponentSync.TransitionPane.Runnable();
+        Core.Scheduler.add(this._runnable); 
+    },
+    
+    _stopTransition: function() {
+        this.doImmediateTransition();
+        Core.Scheduler.add(this._runnable); 
     }
 });
+
+ExtrasRender.ComponentSync.TransitionPane.Runnable = Core.extend(Core.Scheduler.Runnable, {
+
+    transitionPane: null,
+
+    timeInterval: null,
+    
+    _startTime: null,
+    
+    _endTime: null,
+    
+    repeat: true,
+    
+    _initialized: false,
+    
+    $construct: function(transitionPane) {
+        this.transitionPane = transitionPane;
+    },
+    
+    run: function() {
+        if (!this.initialized) {
+            this._startTime = new Date().getTime();
+            this._endTime = this._startTime + this.transitionPane._transitionDuration;
+            this.transitionPane._transition.init();
+            this.initialized = true;
+        } else {
+            var time = new Date().getTime();
+            if (time < this._endTime) {
+                var progress = (time - this._startTime) / this.transitionPane._transitionDuration;
+                this.transitionPane._transition.step(progress);
+            } else {
+                this.transitionPane._transition.dispose();
+                this.transitionPane._stopTransition();
+            }
+        }
+    }
+}); 
 
 ExtrasRender.ComponentSync.TransitionPane.Transition = Core.extend({
 
@@ -85,7 +188,9 @@ ExtrasRender.ComponentSync.TransitionPane.FadeOpacityTransition = Core.extend(
     
     $static: {
     
-        DEFAULT_TRANSITION_DURATION: 1000
+        DEFAULT_TRANSITION_DURATION: 1000,
+        
+        INTERVAL: 10
     },
     
     _transitionPane: null,
@@ -95,34 +200,34 @@ ExtrasRender.ComponentSync.TransitionPane.FadeOpacityTransition = Core.extend(
     },
     
     dispose: function() {
-        if (this._transitionPane.newChildDivElement) {
-            this._transitionPane.newChildDivElement.style.zIndex = 0;
+        if (this._transitionPane.childDivElement) {
+            this._transitionPane.childDivElement.style.zIndex = 0;
             if (WebCore.Environment.PROPRIETARY_IE_OPACITY_FILTER_REQUIRED) {
-                this._transitionPane.newChildDivElement.style.filter = "";
+                this._transitionPane.childDivElement.style.filter = "";
             } else {
-                this._transitionPane.newChildDivElement.style.opacity = 1;
+                this._transitionPane.childDivElement.style.opacity = 1;
             }
         }
-        this.transitionPane.doImmediateTransition();
     },
     
     init: function() {
-        if (this._transitionPane.newChildDivElement) {
+        if (this._transitionPane.childDivElement) {
             if (WebCore.Environment.PROPRIETARY_IE_OPACITY_FILTER_REQUIRED) {
-                this._transitionPane.newChildDivElement.style.filter = "alpha(opacity=0)";
+                this._transitionPane.childDivElement.style.filter = "alpha(opacity=0)";
             } else {
-                this._transitionPane.newChildDivElement.style.opacity = 0;
+                this._transitionPane.childDivElement.style.opacity = 0;
             }
         }
     },
     
     step: function(progress) {
-        if (this._transitionPane.newChildDivElement) {
+Core.Debug.consoleWrite("progress:" +  progress);    
+        if (this._transitionPane.childDivElement) {
             if (WebCore.Environment.PROPRIETARY_IE_OPACITY_FILTER_REQUIRED) {
                 var percent = parseInt(progress * 100);
-                this._transitionPane.newChildDivElement.style.filter = "alpha(opacity=" + percent + ")";
+                this._transitionPane.childDivElement.style.filter = "alpha(opacity=" + percent + ")";
             } else {
-                this._transitionPane.newChildDivElement.style.opacity = progress;
+                this._transitionPane.childDivElement.style.opacity = progress;
             }
         } else if (this._transitionPane.oldChildDivElement) {
             if (WebCore.Environment.PROPRIETARY_IE_OPACITY_FILTER_REQUIRED) {
