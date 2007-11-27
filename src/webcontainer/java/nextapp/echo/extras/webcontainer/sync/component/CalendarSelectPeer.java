800cf6046dad7cafe6ba9b063cdbf2a8e4a97e39
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2007-11-27 13:47:46 +0000

    Dates can be set from server.

--- src/webcontainer/java/nextapp/echo/extras/webcontainer/sync/component/CalendarSelectPeer.java
+++ src/webcontainer/java/nextapp/echo/extras/webcontainer/sync/component/CalendarSelectPeer.java
@@ -1,5 +1,9 @@
 package nextapp.echo.extras.webcontainer.sync.component;
 
+import java.util.Date;
+
+import nextapp.echo.app.Component;
+import nextapp.echo.app.update.ClientUpdateManager;
 import nextapp.echo.app.util.Context;
 import nextapp.echo.extras.app.CalendarSelect;
 import nextapp.echo.extras.webcontainer.service.CommonService;
@@ -20,6 +24,11 @@ extends AbstractComponentSynchronizePeer {
         WebContainerServlet.getServiceRegistry().add(CALENDAR_SELECT_SERVICE);
     }
 
+    public CalendarSelectPeer() {
+        super();
+        addOutputProperty(CalendarSelect.DATE_CHANGED_PROPERTY);
+    }
+    
     /**
      * @see nextapp.echo.webcontainer.ComponentSynchronizePeer#getClientComponentType(boolean)
      */
@@ -35,6 +44,29 @@ extends AbstractComponentSynchronizePeer {
     }
 
     /**
+     * @see nextapp.echo.webcontainer.AbstractComponentSynchronizePeer#getInputPropertyClass(java.lang.String)
+     */
+    public Class getInputPropertyClass(String propertyName) {
+        if (CalendarSelect.DATE_CHANGED_PROPERTY.equals(propertyName)) {
+            return Date.class;
+        }
+        return null;
+    }
+
+    /**
+     * @see nextapp.echo.webcontainer.AbstractComponentSynchronizePeer#getOutputProperty(
+     *      nextapp.echo.app.util.Context, nextapp.echo.app.Component, java.lang.String, int)
+     */
+    public Object getOutputProperty(Context context, Component component, String propertyName, int propertyIndex) {
+        if (propertyName.equals(CalendarSelect.DATE_CHANGED_PROPERTY)) {
+            CalendarSelect calendarSelect = (CalendarSelect) component;
+            return calendarSelect.getDate();
+        } else {
+            return super.getOutputProperty(context, component, propertyName, propertyIndex);
+        }
+    }
+
+    /**
      * @see nextapp.echo.webcontainer.ComponentSynchronizePeer#init(nextapp.echo.app.util.Context)
      */
     public void init(Context context) {
@@ -42,5 +74,15 @@ extends AbstractComponentSynchronizePeer {
         ServerMessage serverMessage = (ServerMessage) context.get(ServerMessage.class);
         serverMessage.addLibrary(CommonService.INSTANCE.getId());
         serverMessage.addLibrary(CALENDAR_SELECT_SERVICE.getId());
-    }    
+    }
+
+    /**
+     * @see nextapp.echo.webcontainer.ComponentSynchronizePeer#storeInputProperty(Context, Component, String, int, Object)
+     */
+    public void storeInputProperty(Context context, Component component, String propertyName, int propertyIndex, Object newValue) {
+        if (propertyName.equals(CalendarSelect.DATE_CHANGED_PROPERTY)) {
+            ClientUpdateManager clientUpdateManager = (ClientUpdateManager) context.get(ClientUpdateManager.class);
+            clientUpdateManager.setComponentProperty(component, CalendarSelect.DATE_CHANGED_PROPERTY, newValue);
+        }
+    }
 }
