55e7e441d2ec33cfde32f216102adbf6296a89ba
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2008-03-04 12:35:30 +0000

    FadeRunnable moved to common library.

--- src/client/extras/Common.Extras.js
+++ src/client/extras/Common.Extras.js
@@ -54,3 +54,45 @@ ExtrasRender = {
         rolloverForeground: "#6f0f0f"
     }
 };
+
+ExtrasRender.FadeRunnable = Core.extend(WebCore.Scheduler.Runnable, {
+
+    timeInterval: 10,
+    repeat: true,
+    _runTime: null,
+    
+    _element: null,
+    _fadeIn: null,
+    _fullOpacity: null,
+    
+    _startTime: null,
+    
+    $construct: function(element, fadeIn, fullOpacity, runTime) {
+        this._element = element;
+        this._fullOpacity = fullOpacity;
+        this._fadeIn = fadeIn;
+        this._runTime = runTime;
+    },
+    
+    run: function() {
+        var time = new Date().getTime();
+        if (this._startTime == null) {
+            this._startTime = time;
+            return;
+        }
+        if (time < this._startTime + this._runTime) {
+            var opacity = ((time - this._startTime) / this._runTime) * this._fullOpacity; 
+            this._element.style.opacity = this._fadeIn ? opacity : this._fullOpacity - opacity;
+        } else {
+            if (this._fadeIn) {
+                this._element.style.opacity = this._fullOpacity;
+            } else {
+                if (this._element.parentNode) {
+                    this._element.parentNode.removeChild(this._element);
+                }
+            }
+            WebCore.Scheduler.remove(this);
+        }
+    }
+});
+
--- src/client/extras/Render.Menu.js
+++ src/client/extras/Render.Menu.js
@@ -191,48 +191,7 @@ ExtrasRender.ComponentSync.Menu.RenderedMenu = Core.extend({
     $static: {
         defaultIconTextMargin: 5,
         defaultMenuInsets: "2px",
-        defaultMenuItemInsets: "1px 12px",
-        
-        FadeRunnable: Core.extend(WebCore.Scheduler.Runnable, {
-        
-            timeInterval: 10,
-            repeat: true,
-            _runTime: null,
-            
-            _element: null,
-            _fadeIn: null,
-            _fullOpacity: null,
-            
-            _startTime: null,
-            
-            $construct: function(element, fadeIn, fullOpacity, runTime) {
-                this._element = element;
-                this._fullOpacity = fullOpacity;
-                this._fadeIn = fadeIn;
-                this._runTime = runTime;
-            },
-            
-            run: function() {
-                var time = new Date().getTime();
-                if (this._startTime == null) {
-                    this._startTime = time;
-                    return;
-                }
-                if (time < this._startTime + this._runTime) {
-                    var opacity = ((time - this._startTime) / this._runTime) * this._fullOpacity; 
-                    this._element.style.opacity = this._fadeIn ? opacity : this._fullOpacity - opacity;
-                } else {
-                    if (this._fadeIn) {
-                        this._element.style.opacity = this._fullOpacity;
-                    } else {
-                        if (this._element.parentNode) {
-                            this._element.parentNode.removeChild(this._element);
-                        }
-                    }
-                    WebCore.Scheduler.remove(this);
-                }
-            }
-        })
+        defaultMenuItemInsets: "1px 12px"
     },
     
     menuSync: null,
@@ -265,8 +224,7 @@ ExtrasRender.ComponentSync.Menu.RenderedMenu = Core.extend({
         } else {
             this.element.style.opacity = 0;
             var fullOpacity = WebCore.Environment.NOT_SUPPORTED_CSS_OPACITY ? 100 : this.component.render("menuOpacity", 100) / 100;
-            var fadeRunnable = new ExtrasRender.ComponentSync.Menu.RenderedMenu.FadeRunnable(this.element, true, fullOpacity, 
-                    animationTime);
+            var fadeRunnable = new ExtrasRender.FadeRunnable(this.element, true, fullOpacity, animationTime);
             WebCore.Scheduler.add(fadeRunnable);
             document.body.appendChild(this.element);
         }
