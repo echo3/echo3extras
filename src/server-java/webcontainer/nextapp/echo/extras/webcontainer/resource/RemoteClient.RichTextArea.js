3426f165efc43acc665526e9d92fce21311adeb9
n.beekman <n.beekman@06d4db22-c32e-0410-96b4-888c99e491d5>
2008-04-03 13:45:43 +0000

    Removed icon overriding code, this can be done by specifying the "icon" property.

--- src/server-java/webcontainer/nextapp/echo/extras/webcontainer/resource/RemoteClient.RichTextArea.js
+++ src/server-java/webcontainer/nextapp/echo/extras/webcontainer/resource/RemoteClient.RichTextArea.js
@@ -14,54 +14,5 @@ ExtrasRender.ComponentSync.RemoteRichTextArea = Core.extend(ExtrasRender.Compone
 
     $load: function() {
         EchoRender.registerPeer("ExtrasApp.RemoteRichTextArea", this);
-    },
-
-    getIcons: function() {
-        var icons = ExtrasRender.ComponentSync.RichTextArea.prototype.getIcons.call(this);
-        return icons ? icons : this._defaultIcons;
-    },
-    
-    renderAdd: function(update, parentElement) {
-        if (!this._defaultIcons) {
-            this._installIcons();
-        }
-        ExtrasRender.ComponentSync.RichTextArea.prototype.renderAdd.call(this, update, parentElement);
-    },
-    
-    _installIcons: function() {
-        var imageServiceUrl = this.client.getResourceUrl("Echo.Image");
-        var iconNames = [
-            "undo",
-            "redo",
-            "cut",
-            "copy",
-            "paste",
-            "bulletedList",
-            "numberedList",
-            "table",
-            "image",
-            "horizontalRule",
-            "hyperlink",
-            "bold", 
-            "italic", 
-            "underline",
-            "superscript",
-            "subscript",
-            "indent",
-            "outdent",
-            "foreground",
-            "background",
-            "alignmentLeft",
-            "alignmentCenter",
-            "alignmentRight",
-            "alignmentJustify"
-        ];
-        this._defaultIcons = {};
-        for (var i = 0; i < iconNames.length; ++i) {
-            this._defaultIcons[iconNames[i]] = imageServiceUrl + "&iid=EchoExtras.RemoteRichTextArea." + iconNames[i];
-        }
-        
-        this._defaultIcons.ok = imageServiceUrl + "&iid=EchoExtras.CommonImages.ok";
-        this._defaultIcons.cancel = imageServiceUrl + "&iid=EchoExtras.CommonImages.cancel";
     }
 });
