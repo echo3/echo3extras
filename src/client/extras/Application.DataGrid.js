086128e11449a229463930b7bb8d3da42f1310f9
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2009-01-23 20:53:09 +0000

    Docs.

--- src/client/extras/Application.DataGrid.js
+++ src/client/extras/Application.DataGrid.js
@@ -1,16 +1,26 @@
 /**
- * DataGrid component.
- *
- * This is an EXPERIMENTAL component, it should not be used at this point for any purpose other than testing it.
- *
+ * DataGrid component: a model-based container which can display extremely large
+ * amounts of content in a scrollable view. Content is retrieved from the model
+ * only as necessary, enabling the component to contain more content than could
+ * ever possibly be rendered. This component renders its content using cell
+ * renderers, not child components. This component may not contain child
+ * components.
+ * 
+ * This is an EXPERIMENTAL component, it should not be used at this point for
+ * any purpose other than testing it.
+ * 
  * @cp {Extras.DataGrid.Model} model the data model
  * @cp {Number} rowIndex displayed origin row index
  * @cp {Number} columnIndex displayed origin column index
  * @cp {Border} cellBorder default cell border
- * @sp {Number} fixedRowsTop the number of rows at the top which should not scroll
- * @sp {Number} fixedRowsBottom the number of rows at the bottom which should not scroll
- * @sp {Number} fixedColumnsRight the number of columns on the right side which should not scroll
- * @sp {Number} fixedColumnsLeft the number of columns on the left side which should not scroll
+ * @sp {Number} fixedRowsTop the number of rows at the top which should not
+ *     scroll
+ * @sp {Number} fixedRowsBottom the number of rows at the bottom which should
+ *     not scroll
+ * @sp {Number} fixedColumnsRight the number of columns on the right side which
+ *     should not scroll
+ * @sp {Number} fixedColumnsLeft the number of columns on the left side which
+ *     should not scroll
  * @sp {Array} columnWidth the widths of columns (as Extents)
  */
 Extras.DataGrid = Core.extend(Echo.Component, {
@@ -19,21 +29,45 @@ Extras.DataGrid = Core.extend(Echo.Component, {
         Echo.ComponentFactory.registerType("Extras.DataGrid", this);
     },
     
+    /** @see Echo.Component#componentType */
     componentType: "Extras.DataGrid",
     
+    /** @see Echo.Component#pane */
     pane: true
 });
 
+/**
+ * Abstract base class for <code>DataGrid</code> models.
+ */
 Extras.DataGrid.Model = Core.extend({
 
     $abstract: {
     
+        /**
+         * Returns the data value contained in the model at the specified column and row.
+         * The value will be provided to a renderer before it is displayed.
+         * The returned value may be of any type.
+         * 
+         * @param {Number} column the column number (0-based)
+         * @param {Number} row the row number (0-based)
+         * @return the model value
+         */
         get: function(column, row) { },
     
-        getColumnCount: function() {
-        },
+        /**
+         * Returns the number of columns in the model
+         * 
+         * @return the number of columns in the model
+         * @type Number
+         */
+        getColumnCount: function() { },
         
-        getRowCount: function() {
-        }
+        /**
+         * Returns the number of rows in the model
+         * 
+         * @return the number of rows in the model
+         * @type Number
+         */
+        getRowCount: function() { }
     }
 });
