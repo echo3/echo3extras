72936cb792e5c3d31c2af6913350a3d77014c882
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2009-02-17 07:07:42 +0000

    Docs.

--- src/client/extras/Sync.TransitionPane.js
+++ src/client/extras/Sync.TransitionPane.js
@@ -7,32 +7,56 @@ Extras.Sync.TransitionPane = Core.extend(Echo.Render.ComponentSync, {
         Echo.Render.registerPeer("Extras.TransitionPane", this);
     },
 
+    /**
+     * Outermost/top-level container element.
+     * @type Element
+     */
     _containerDiv: null,
+    
+    /**
+     * Content element, contains oldChildDiv/childDiv elements.
+     * @type Element
+     */
     contentDiv: null,
+    
+    /**
+     * The transition type value (retrieved from the component).
+     * @type Number
+     */
     type: null,
-    _duration: null,
+    
+    /**
+     * The transition which is actively running (null when content is not being transitioned).
+     * @type Extras.Sync.TransitionPane.Transition
+     */
     _transition: null,
+    
+    /**
+     * Reference to the Extras.Sync.TransitionPane.Transition object type which will be instantiated to perform a transition.
+     */
     _transitionClass: null,
-    _runnable: null,
     
     /**
      * The element containing the old child element, which is being transitioned FROM.
+     * @type Element
      */
     oldChildDiv: null,
     
     /**
      * The element containing the current/new child element, which is being transitioned TO.
+     * @type Element
      */
     childDiv: null,
     
     /**
      * Flag indicating whether initial content has been loaded (no transition effect is used on the first load).
+     * @type Boolean
      */
     _initialContentLoaded: false,
 
-    $construct: function() {
-    },
-    
+    /**
+     * Performs an immediate transition between old content and new content with no animated effect.
+     */
     doImmediateTransition: function() {
         this.removeOldContent();
         if (this.childDiv) {
@@ -40,6 +64,10 @@ Extras.Sync.TransitionPane = Core.extend(Echo.Render.ComponentSync, {
         }
     },
 
+    /**
+     * Determines the transition class that will be used to change content based on the
+     * type value of the supported Extras.TransitionPane component. 
+     */
     _loadTransition: function() {
         this.type = this.component.render("type");
         switch (this.type) {
@@ -62,7 +90,6 @@ Extras.Sync.TransitionPane = Core.extend(Echo.Render.ComponentSync, {
             break;
         default:
             this._transitionClass = null;
-            this._duration = null;
         }
     },
     
@@ -76,12 +103,6 @@ Extras.Sync.TransitionPane = Core.extend(Echo.Render.ComponentSync, {
         }
     },
 
-    _hideContent: function() {
-        if (this.childDiv) {
-            this.childDiv.style.visibility = "hidden";
-        }
-    },
-    
     /**
      * Shows new content.
      */
@@ -107,6 +128,11 @@ Extras.Sync.TransitionPane = Core.extend(Echo.Render.ComponentSync, {
         }
     },
     
+    /**
+     * Renders new content (a new child) added in an update.  Starts the transition.
+     * 
+     * @param {Echo.Update.ComponentUpdate} the update 
+     */
     _renderAddChild: function(update) {
         this._loadTransition();
         this.childDiv = document.createElement("div");
@@ -115,7 +141,7 @@ Extras.Sync.TransitionPane = Core.extend(Echo.Render.ComponentSync, {
         Echo.Render.renderComponentAdd(update, this.component.children[0], this.childDiv);
         
         if (this._initialContentLoaded) {
-            this._hideContent();
+            this.childDiv.style.visibility = "hidden";
             if (this._transitionClass) {
                 this._transitionStart();
             } else {
@@ -184,12 +210,18 @@ Extras.Sync.TransitionPane = Core.extend(Echo.Render.ComponentSync, {
         return fullRender;
     },
     
+    /**
+     * Initiates the animated transition effect.
+     */
     _transitionStart: function() {
         this._transition = new this._transitionClass(this);
         this._transition.runTime = this.component.render("duration", this._transition.runTime);
         this._transition.start(Core.method(this, this._transitionFinish));
     },
     
+    /**
+     * Completes the animated transition effect. 
+     */
     _transitionFinish: function(abort) {
         // Abort current transition, if necessary.
         if (this._transition) {
@@ -260,9 +292,28 @@ Extras.Sync.TransitionPane.BlindTransition = Core.extend(Extras.Sync.TransitionP
     /** @see Extras.Sync.Animation#runTime */
     runTime: 700,
 
+    /**
+     * The mask DIV that will display the blind graphic effect over the content.
+     * @type Element
+     */
     _maskDiv: null,
+    
+    /**
+     * Number of steps (images) to display.
+     * @type Number
+     */
     _stepCount: 14,
+    
+    /**
+     * Step number where old content will be swapped for new content.
+     * @type Number
+     */
     _swapStep: null,
+    
+    /**
+     * Flag indicating whether the transition will occur in reverse order.
+     * @type Boolean
+     */
     _reverse: false,
     
     /** @see Extras.Sync.Animation#complete */
@@ -327,9 +378,17 @@ Extras.Sync.TransitionPane.BlindTransition = Core.extend(Extras.Sync.TransitionP
  */
 Extras.Sync.TransitionPane.CameraPanTransition = Core.extend(
         Extras.Sync.TransitionPane.Transition, {
-        
+    
+    /**
+     * Flag indicating whether the new child (being transitioned to) has been placed on the screen.
+     * @type Boolean
+     */
     _newChildOnScreen: false,
     
+    /**
+     * The distance, in pixels, which content will travel across the screen (the width/height of the region).
+     * @type Number 
+     */
     _travel: null,
 
     /** @see Extras.Sync.Animation#complete */
