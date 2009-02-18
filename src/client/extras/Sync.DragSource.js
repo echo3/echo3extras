9cc45c519294e51f607290195bcbb749fe545131
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2009-02-18 02:53:39 +0000

    DND progress.

--- src/client/extras/Sync.DragSource.js
+++ src/client/extras/Sync.DragSource.js
@@ -8,10 +8,11 @@ Extras.Sync.DragSource = Core.extend(Echo.Render.ComponentSync, {
     },
     
     /**
-     * Clone of rendered DIV element (and descendant hierarchy).  Used when dragging the object.
+     * The dragging element.  This element is created by cloning the rendered DIV (and its descendants), reducing their
+     * opacity, and then absolutely positioning it adjacent the mouse cursor position.
      * @type Element
      */
-    _cloneDiv: null,
+    _dragDiv: null,
     
     /**
      * Rendered DIV element.
@@ -19,23 +20,68 @@ Extras.Sync.DragSource = Core.extend(Echo.Render.ComponentSync, {
      */
     _div: null,
     
-    _dragStart: function() {
-        this._dragStop();
-        this._cloneDiv = this._div.cloneNode(true);
-        this._cloneDiv.style.position = "absolute";
-        this._cloneDiv.style.zIndex = "32767";
-        this._setCloneOpacity(0.2);
-        document.body.appendChild(this._cloneDiv);
+    /**
+     * Overlay DIV which covers other elements (such as IFRAMEs) when dragging which may otherwise suppress events.
+     * @type Element
+     */
+    _overlayDiv: null,
+    
+    _divOrigin: null,
+
+    /**
+     * Method reference to <code>_processMouseMove()</code>.
+     * @type Function
+     */
+    _processMouseMoveRef: null,
+
+    /**
+     * Method reference to <code>_processMouseUp()</code>.
+     * @type Function
+     */
+    _processMouseUpRef: null,
+
+    $construct: function() {
+        this._processMouseMoveRef = Core.method(this, this._processMouseMove);
+        this._processMouseUpRef = Core.method(this, this._processMouseUp);
     },
     
+    /**
+     * Start a drag operation.
+     */
+    _dragStart: function() {
+        this._dragStop();
+        
+        this._overlayDiv = document.createElement("div");
+        this._overlayDiv.style.cssText = "position:absolute;z-index:32767;width:100%;height:100%;";
+        Echo.Sync.FillImage.render(this.client.getResourceUrl("Echo", "resource/Transparent.gif"), this._overlayDiv);
+
+        this._divOrigin = new Core.Web.Measure.Bounds(this._div);
+        
+        this._dragDiv = this._div.cloneNode(true);
+        this._dragDiv.style.position = "absolute";
+        this._dragDiv.style.position.top = this._divOrigin.left + "px";
+        this._dragDiv.style.position.left = this._divOrigin.top + "px";
+        this._setDragOpacity(0.2);
+        this._overlayDiv.appendChild(this._dragDiv);
+
+        document.body.appendChild(this._overlayDiv);
+
+        Core.Web.Event.add(document.body, "mousemove", this._processMouseMoveRef, true);
+        Core.Web.Event.add(document.body, "mouseup", this._processMouseUpRef, true);
+    },
+    
+    /**
+     * Stop a drag operation.
+     */
     _dragStop: function() {
-        if (!this._cloneDiv) {
-            return;
+        Core.Web.Event.remove(document.body, "mousemove", this._processMouseMoveRef, true);
+        Core.Web.Event.remove(document.body, "mouseup", this._processMouseUpRef, true);
+
+        if (this._overlayDiv) {
+            document.body.removeChild(this._overlayDiv);
+            this._overlayDiv = null;
         }
-        if (this._cloneDiv.parentNode) {
-            this._cloneDiv.parentNode.removeChild(this._cloneDiv);
-        }
-        this._cloneDiv = null;
+        this._dragDiv = null;
     },
     
     _processMouseDown: function(e) {
@@ -46,6 +92,13 @@ Extras.Sync.DragSource = Core.extend(Echo.Render.ComponentSync, {
         }
         
         this._dragStart();
+    },
+    
+    _processMouseMove: function(e) {
+    },
+    
+    _processMouseUp: function(e) {
+        this._dragStop();
     },
     
     /** @see Echo.Render.ComponentSync#renderAdd */
@@ -65,7 +118,7 @@ Extras.Sync.DragSource = Core.extend(Echo.Render.ComponentSync, {
         this._dragStop();
         Core.Web.Event.removeAll(this._div);
 
-        this._cloneDiv = null;
+        this._dragDiv = null;
         this._div = null;
     },
     
@@ -78,13 +131,13 @@ Extras.Sync.DragSource = Core.extend(Echo.Render.ComponentSync, {
         this.renderAdd(update, containerElement);
     },
     
-    _setCloneOpacity: function(value) {
+    _setDragOpacity: function(value) {
         if (Core.Web.Env.NOT_SUPPORTED_CSS_OPACITY) {
             if (Core.Web.Env.PROPRIETARY_IE_OPACITY_FILTER_REQUIRED) {
-                this._cloneDiv.style.filter = "alpha(opacity=" + (value * 100) + ")";
+                this._dragDiv.style.filter = "alpha(opacity=" + (value * 100) + ")";
             }
         } else {
-            this._cloneDiv.style.opacity = value;
+            this._dragDiv.style.opacity = value;
         }
     }
 });
