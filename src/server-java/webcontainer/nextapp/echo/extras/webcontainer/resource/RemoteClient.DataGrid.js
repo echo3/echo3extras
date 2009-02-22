f5ed88170e482bff0592254766f2935a4e4b0335
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2009-02-22 05:32:19 +0000

    Progress on server-based data model for remote data grids.

--- src/server-java/webcontainer/nextapp/echo/extras/webcontainer/resource/RemoteClient.DataGrid.js
+++ src/server-java/webcontainer/nextapp/echo/extras/webcontainer/resource/RemoteClient.DataGrid.js
@@ -2,11 +2,64 @@
  * RemoteClient-hosted DataGrid component.
  */
 Extras.RemoteDataGrid = Core.extend(Extras.DataGrid, {
+    
+    $static: {
+
+        /**
+         * Server-based incrementally provided model.
+         */
+        Model: Core.extend(Extras.DataGrid.Model, {
+            
+            /**
+             * Column count.
+             * @type Number
+             */
+            _columnCount: null,
+            
+            /**
+             * Row count.
+             * @type Number
+             */
+            _rowCount: null,
+            
+            /**
+             * Constructor.
+             * 
+             * @param {Number} columnCount the column count
+             * @param {Number} rowCount the row count
+             */
+            $construct: function(columnCount, rowCount) {
+                this._columnCount = columnCount;
+                this._rowCount = rowCount;
+            },
+            
+            /** @see Extras.DataGrid.Model#get */
+            get: function(column, row) {
+                return null;
+            },
+            
+            /** @see Extras.DataGrid.Model#getColumnCount */
+            getColumnCount: function() {
+                return this._columnCount;
+            },
+            
+            /** @see Extras.DataGrid.Model#getRowCount */
+            getRowCount: function() {
+                return this._rowCount;
+            },
+            
+            /** @see Extras.DataGrid.Model#prefetch */
+            prefetch: function(callback, firstColumn, firstRow, lastColumn, lastRow) {
+                callback();
+            }
+        })
+    },
 
     $load: function() {
         Echo.ComponentFactory.registerType("Extras.RemoteDataGrid", this);
     },
     
+    /** @see Echo.Component#componentType */
     componentType: "Extras.RemoteDataGrid"
 });
 
@@ -15,6 +68,10 @@ Extras.RemoteDataGrid = Core.extend(Extras.DataGrid, {
  */
 Extras.Sync.RemoteDataGrid = Core.extend(Extras.Sync.DataGrid, {
 
+    $static: {
+    
+    },
+    
     $load: function() {
         Echo.Render.registerPeer("Extras.RemoteDataGrid", this);
     }
