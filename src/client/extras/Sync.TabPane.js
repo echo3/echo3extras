a53570e2089397e387932092e096ed749b7b7e8c
tliebeck <tliebeck@06d4db22-c32e-0410-96b4-888c99e491d5>
2009-10-17 03:01:20 +0000

    Updated TabPane image monitoring code to avoid recursive creations of image monitors.

--- src/client/extras/Sync.TabPane.js
+++ src/client/extras/Sync.TabPane.js
@@ -282,6 +282,11 @@ Extras.Sync.TabPane = Core.extend(Echo.Render.ComponentSync, {
     _layoutRequired: false,
     
     /**
+     * Flag indicating whether header images have been completely loaded.
+     */
+    _headerImageLoadingComplete: false,
+    
+    /**
      * Flag indicating whether a full header re-render operation is required.  Flag is set by renderUpdate() method in response
      * to child layout data changes to avoid full render.
      * @type Boolean
@@ -375,14 +380,18 @@ Extras.Sync.TabPane = Core.extend(Echo.Render.ComponentSync, {
             this._tabs[i]._renderDisplay();
         }
         
-        // Add image monitor to re-execute renderLayout as images are loaded.
-        var imageListener = Core.method(this, function() {
-            if (this.component) { // Verify component still registered.
-                this._layoutRequired = true;
-                this._renderLayout();
-            }
-        });
-        Core.Web.Image.monitor(this._headerContainerDiv, imageListener);
+        if (!this._headerImageLoadingComplete) {
+            this._headerImageLoadingComplete = true;
+            // Add image monitor to re-execute renderLayout as images are loaded.
+            var imageListener = Core.method(this, function() {
+                if (this.component) { // Verify component still registered.
+                    this._layoutRequired = true;
+                    this._renderLayout();
+                }
+            });
+            imageListener.id = "TabPane:" + this.component.renderId;
+            Core.Web.Image.monitor(this._headerContainerDiv, imageListener);
+        }
     },
     
     /**
@@ -632,6 +641,8 @@ Extras.Sync.TabPane = Core.extend(Echo.Render.ComponentSync, {
     
     /** @see Echo.Render.ComponentSync#renderAdd */
     renderAdd: function(update, parentElement) {
+        this._headerImageLoadingComplete = false;
+
         this.component.addListener("tabSelect", this._tabSelectListenerRef);
         
         // Store rendering properties.
@@ -863,6 +874,8 @@ Extras.Sync.TabPane = Core.extend(Echo.Render.ComponentSync, {
         var fullRender = false,
             tab,
             i;
+        
+        this._headerImageLoadingComplete = false;
         
         if (update.hasUpdatedLayoutDataChildren()) {
             this._headerUpdateRequired = true;
